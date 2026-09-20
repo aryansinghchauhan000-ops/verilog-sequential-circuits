a65034b4263088c02c869dc6274dc60e1dd5a0c9
Aryan Chauhan <aryansinghchauhan000@gmail.com>
2026-09-20 16:09:30 +0530

    JK_FF_Async_Reset_RTL.v

---
+++ JK_FF/JK_FF_Async_Reset/JK_FF_Async_Reset_RTL.v
@@ -0,0 +1,20 @@
+module JK_FF_Async_Reset(j,k,clk,rst,q);
+  input j,k,clk,rst;
+  output reg q;
+
+  always @(posedge clk or negedge rst)
+    begin
+      if(!rst)
+        q < =1'b0;
+      else
+        begin
+          case({j,k})
+            2'b00 : q <= q;
+            2'b01 : q <= 0;
+            2'b10 : q <= 1;
+            2'b11 : q <= ~q;
+            default : q <= 1'bx;
+          endcase
+        end
+    end
+endmodule  
