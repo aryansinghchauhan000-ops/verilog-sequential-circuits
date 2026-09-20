e728af84b05f09de7df7c249361634511d40d17a
Aryan Chauhan <aryansinghchauhan000@gmail.com>
2026-09-20 13:47:55 +0530

    JK_FF_Basic_RTL.v

---
+++ JK_FF/JK_FF_Basic/JK_FF_Basic_RTL.v
@@ -0,0 +1,15 @@
+module JK_FF_Basic(j,k,clk,q);
+  input j,k,clk;
+  output reg q;
+  
+  always @(posedge clk)
+    begin
+      case({j,k})
+        2'b00 : q <= q;
+        2'b01 : q <= 0;
+        2'b10 : q <= 1;
+        2'b11 : q <= ~q;
+        default : q <= 1'bx;
+      endcase
+    end
+endmodule        
