642b03df6a9fcd3df2659f48937102657352404a
Aryan Chauhan <aryansinghchauhan000@gmail.com>
2026-09-20 19:02:28 +0530

    D_FF_Sync_Reset_RTL.v

---
+++ D_FF/D_FF_Sync_Reset/D_FF_Sync_Reset_RTL.v
@@ -0,0 +1,12 @@
+module D_FF_Sync_Reset(d,clk,rst,q);
+  input d,clk,rst;
+  output reg q;
+
+  always @(posedge clk)
+    begin
+      if(rst)
+        q <= 1'b0;
+      else
+        q <= d;
+    end
+endmodule
