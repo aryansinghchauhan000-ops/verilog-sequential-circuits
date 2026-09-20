792ec83b42ecd8c9b0fc9c4a463bdf71b21eeb83
Aryan Chauhan <aryansinghchauhan000@gmail.com>
2026-09-20 12:18:16 +0530

    SR_FF_Async_Reset_TB.v

---
+++ SR_FF/SR_FF_Async_Reset/SR_FF_Async_Reset_TB.v
@@ -0,0 +1,29 @@
+module tb;
+  reg s,r,clk,rst;
+  wire q;
+
+  SR_FF_Async_Reset abc(s,r,clk,rst,q);
+
+  always #5 clk = ~clk;
+
+  initial begin
+    clk = 0;
+    rst = 0;
+    s = 0;
+    r = 0;
+
+    $monitor("Time = %0t, clk = %b, rst = %b, s = %b, r = %b, q = %b", $time, clk, rst, s, r, q);
+    #10;
+    rst = 1;
+    for(int k = 0; k < 5; k++)
+      begin
+        {s,r} = k;
+        #10;
+      end
+    $finish;
+  end
+  initial begin
+    $dumpfile("dump.vcd");
+    $dumpvars(0,tb);
+  end
+endmodule
