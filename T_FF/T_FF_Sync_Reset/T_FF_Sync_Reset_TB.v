5b93919a7c123baf0f9cdeb0881755a497b7ccb7
Aryan Chauhan <aryansinghchauhan000@gmail.com>
2026-09-21 12:22:24 +0530

    T_FF_Sync_Reset_TB.v

---
+++ T_FF/T_FF_Sync_Reset/T_FF_Sync_Reset_TB.v
@@ -0,0 +1,28 @@
+module tb;
+  reg t,clk,rst;
+  wire q;
+
+  T_FF_Sync_Reset abc(t,clk,rst,q);
+
+  always #5 clk = ~clk;
+  initial begin
+    clk = 0;
+    rst = 1;
+    t = 0;
+
+    $monitor("Time = %0t, clk = %b, rst = %b, t = %b, q =%b", $time, clk, rst, t, q);
+    #10;
+    rst = 0;
+    
+    for(int i = 0; i < 3; i++)
+      begin
+        t = i;
+        #10;
+      end
+    $finish;
+  end
+  initial begin
+    $dumpfile("dump.vcd");
+    $dumpvars(0,tb);
+  end
+endmodule
