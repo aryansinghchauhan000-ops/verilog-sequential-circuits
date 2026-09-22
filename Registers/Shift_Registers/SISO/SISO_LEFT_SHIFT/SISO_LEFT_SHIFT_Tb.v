f64eb29d12bacc27aa3575e7ce257bc46d538866
Aryan Chauhan <aryansinghchauhan000@gmail.com>
2026-09-22 07:40:27 +0530

    SISO_LEFT_SHIFT_Tb.v

---
+++ Registers/Shift_Registers/SISO/SISO_LEFT_SHIFT/SISO_LEFT_SHIFT_Tb.v
@@ -0,0 +1,49 @@
+module tb;
+  reg clk, rst, clr, ld, en, d_in, s_in;
+  reg [3:0]d;
+  wire [3:0] q;
+  wire s_out;
+  
+  SISO abc(clk, rst, clr, ld, en, d, s_in, s_out, q);
+  
+  always #5 clk = ~clk;
+  
+  initial begin
+    clk = 0;
+    rst = 0;
+    clr = 0;
+    ld  = 0;
+    en  = 0;
+    d   = 0;
+    
+    $monitor("Time = %0t, clk=%b, rst=%b, clr=%b, ld=%b, en=%b, d=%b, s_in=%b, s_out=%b, q=%b", $time,clk, rst, clr, ld, en, d, s_in, s_out, q);
+    
+    #10;
+    rst = 1;
+    
+    #10;
+    ld  = 1;
+    en  = 1;
+    d   = 4'b1010;
+    
+    #10;
+    ld  = 0;
+    s_in = 1'b1;
+    
+    #10;
+    s_in = 1'b1;
+    s_in = 1'b1;
+    
+    #10;
+    s_in = 1'b1;
+    
+    #20;
+    $finish;
+  end
+  
+  initial begin
+    $dumpfile("dump.vcd");
+    $dumpvars(0,tb);
+  end
+endmodule
+    
