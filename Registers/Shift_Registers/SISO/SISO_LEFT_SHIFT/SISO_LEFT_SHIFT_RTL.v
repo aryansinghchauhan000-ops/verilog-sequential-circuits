18dcad1e4eef5cd5e6355faa02fd3d41c0ea78b5
Aryan Chauhan <aryansinghchauhan000@gmail.com>
2026-09-22 20:37:49 +0530

    SISO_LEFT_SHIFT_RTL.v

--- Registers/Shift_Registers/SISO/SISO_LEFT_SHIFT/SISO_LEFT_SHIFT_RTL.v
+++ Registers/Shift_Registers/SISO/SISO_LEFT_SHIFT/SISO_LEFT_SHIFT_RTL.v
@@ -1,10 +1,10 @@
-module SISI(clk, rst, clr, ld, en, d, s_in, s_out, q);
+module SISO(clk, rst, clr, ld, en, d, s_in, s_out, q);
   input [3:0]d;
   input clk,rst,clr,ld,en, s_in;
   output reg [3:0] q;
   output s_out;
   
-  assign s_out = q[0];
+  assign s_out = q[3];
   
   always @(posedge clk or negedge rst)
     begin
@@ -22,6 +22,6 @@ module SISI(clk, rst, clr, ld, en, d, s_in, s_out, q);
         q <= d;
         
       else if(en)
-        q <= {s_in, q[3:1]}; 
+        q <= {q[2:0], s_in};
     end
 endmodule
