265c133d195d83e5ae4ad15e3beabcac16614d5a
Aryan Chauhan <aryansinghchauhan000@gmail.com>
2026-09-22 18:07:35 +0530

    SISO_RIGHT_SHIFT_RTL.v

--- Registers/Shift_Registers/SISO/SISO_RIGHT_SHIFT/SISO_RIGHT_SHIFT_RTL.v
+++ Registers/Shift_Registers/SISO/SISO_RIGHT_SHIFT/SISO_RIGHT_SHIFT_RTL.v
@@ -4,4 +4,17 @@ module SISO_RIGHT_SHIFT(clk,rst,clr,ld,en,d,s_in,s_out,q);
   output reg [3:0]q;
   output s_out;
 
-  
+  assign s_out = q[3];
+
+  always @(posedge clk or negedge rst)
+    begin
+      if(!rst)
+        q <= 4'b0000;
+      else if(clr)
+        q <= 4'b0000;
+      else if( en && ld)
+        q <= d;
+      else if(en)
+        q <= {q[2:0], s_in};
+    end
+endmodule
