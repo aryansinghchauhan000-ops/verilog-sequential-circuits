f7764ad1705f79b669cbb5114e0451fbb498c170
Aryan Chauhan <aryansinghchauhan000@gmail.com>
2026-09-22 19:27:22 +0530

    SISO_RIGHT_SHIFT_TB.v

--- Registers/Shift_Registers/SISO/SISO_RIGHT_SHIFT/SISO_RIGHT_SHIFT_TB.v
+++ Registers/Shift_Registers/SISO/SISO_RIGHT_SHIFT/SISO_RIGHT_SHIFT_TB.v
@@ -1,24 +1,26 @@
 module tb;
+
   reg clk, rst, clr, ld, en, s_in;
-  reg [3:0]d;
-  wire [3:0]q;
+  reg [3:0] d;
+
+  wire [3:0] q;
   wire s_out;
 
   SISO_RIGHT_SHIFT abc(clk, rst, clr, ld, en, d, s_in, s_out, q);
 
-  always #5 clk= ~ clk;
+  always #5 clk = ~clk;
 
   initial begin
-    clk = 0;
-    rst = 1;
-    clr = 0;
-    ld  = 0;
-    en  = 0;
-    d  = 4'b0000;
 
-    $monitor("Time = %0t, clk = %b, rst = %b, clr = %b, ld = %b, en = %b, d = %b, s_in = %b, s_out = %b, q = %b",$time, clk, rst, clr, ld, en, d, s_in, s_out, q);
-    #10;
-    rst = 1;
+    clk  = 0;
+    rst  = 1;
+    clr  = 0;
+    ld   = 0;
+    en   = 0;
+    s_in = 0;
+    d    = 4'b0000;
+
+    $monitor("Time=%0t clk=%b rst=%b clr=%b ld=%b en=%b d=%b s_in=%b s_out=%b q=%b",$time, clk, rst, clr, ld, en, d, s_in, s_out, q);
 
     #10;
     ld = 1;
@@ -26,17 +28,17 @@ module tb;
     d = 4'b1010;
 
     #10;
-
     ld = 0;
-    s_in = 1'b1;
+    s_in = 1;
 
     #10;
-    s_in = 1'b1;
+    s_in = 1;
 
     #10;
     clr = 1;
 
-    #10
+    #10;
+    clr = 0;
     ld = 1;
     d = 4'b1111;
 
@@ -45,19 +47,20 @@ module tb;
     s_in = 0;
 
     #10;
-    s_in =0;
-    
+    s_in = 0;
+
     #10;
     rst = 0;
 
     #10;
     rst = 1;
-    
+
     #10;
     $finish;
   end
+
   initial begin
     $dumpfile("dump.vcd");
-    $dumpvars(0,tb);
+    $dumpvars(0, tb);
   end
 endmodule
