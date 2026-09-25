d5723bc181a86d4c9d8ff4ed85f0d4b381297034
Aryan Chauhan <aryansinghchauhan000@gmail.com>
2026-09-25 19:49:44 +0530

    Synchronous_Down_Counter_RTL.v

---
+++ COUNTERS/Synchronous_Counter/Synchronous_Down_Counter/Synchronous_Down_Counter_RTL.v
@@ -0,0 +1,12 @@
+module Synchronous_Down_Counter(clk, rst, en, q);
+  input clk, rst, en;
+  output reg [3:0] q;
+  
+  always @(posedge clk or negedge rst)
+    begin
+      if(!rst)
+        q <= 4'b0000;
+      else if(en)
+        q <= q-1;
+    end
+endmodule
