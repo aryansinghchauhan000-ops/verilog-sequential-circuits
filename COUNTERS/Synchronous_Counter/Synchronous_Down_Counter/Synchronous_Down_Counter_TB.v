ca7feaf72c0f798575b2358fdc98fcee6e055d52
Aryan Chauhan <aryansinghchauhan000@gmail.com>
2026-09-25 19:50:19 +0530

    Synchronous_Down_Counter_TB.v

---
+++ COUNTERS/Synchronous_Counter/Synchronous_Down_Counter/Synchronous_Down_Counter_TB.v
@@ -0,0 +1,31 @@
+module tb;
+  reg clk, rst, en;
+  wire [3:0]q;
+  
+  Synchronous_Down_Counter abc(clk, rst, en, q);
+  
+  always #5 clk = ~clk;
+  initial begin
+    clk = 0;
+    rst = 0;
+    en  = 0;
+    
+    $monitor("Time =%0t, clk =%b, rst =%b, en =%b, q=%b ", $time, clk, rst, en, q);
+ 
+    #10;  
+    rst = 1;
+  
+    #10;
+    en = 1;
+  
+    #160;
+    $finish;  
+    
+    #10;
+  end
+  initial begin
+    $dumpfile("dump.vcd");
+    $dumpvars(0,tb);
+  end  
+endmodule
+  
