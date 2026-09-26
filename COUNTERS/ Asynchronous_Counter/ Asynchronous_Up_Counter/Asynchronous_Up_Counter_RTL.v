ab0a7a3a265f04c3db0eb602b073aee215036ae8
Aryan Chauhan <aryansinghchauhan000@gmail.com>
2026-09-26 20:31:58 +0530

    Asynchronous_Up_Counter_RTL.v
    Added asynchronous up counter module with reset functionality.

--- COUNTERS/ Asynchronous_Counter/ Asynchronous_Up_Counter/Asynchronous_Up_Counter_RTL.v
+++ COUNTERS/ Asynchronous_Counter/ Asynchronous_Up_Counter/Asynchronous_Up_Counter_RTL.v
@@ -1,1 +1,16 @@
+module Asynchronous_Up_Counter(tff, clk, q);
+  input [3:0]tff;
+  input clk;
+  output reg [3:0] q;
 
+  always @(posedge clk or negedge rst)
+    begin
+      if(!rst)
+        q <= 4'b000;
+      else
+        q <= ~t;
+    end
+
+  always @(negedge q[0] or negedge rst)
+    begin
+      if(
